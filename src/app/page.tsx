a999dd20449fa36a3d2305bc1337ed581eeb9f41
Naman Dadhich <namandadhich15592@gmail.com>
2025-04-13 22:40:09 +0530

    heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import EmailForm from "@/components/EmailForm";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-orange-600 font-mono p-8">
-      <h1 className="text-5xl font-bold">T.P*</h1>
-      <p className="uppercase">Terminal | Protocol</p>
+      <h1 className="text-5xl font-bold">bulkMailer</h1>
+      <p className="mt-2">Win thousands of hearts at once...</p>
       <hr className="border-orange-600 my-4" />
       <EmailForm />
     </main>
